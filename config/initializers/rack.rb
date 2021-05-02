00c131cc931f5daedb132e65ca1677cf0f2f7dc0
Georg Ledermann <georg@ledermann.dev>
2021-05-02 11:40:18 +0200

    Simplify and test gzip compression with Rack::Deflater

--- config/initializers/rack.rb
+++ config/initializers/rack.rb
@@ -4,3 +4,6 @@ Rails.application.config.middleware.delete(Rack::Runtime)
 # Add content_type for some file extension not included in the
 # list of defaults: https://github.com/rack/rack/blob/master/lib/rack/mime.rb
 Rack::Mime::MIME_TYPES['.webmanifest'] = 'application/manifest+json'
+
+# Enable gzip compression
+Rails.application.config.middleware.use Rack::Deflater
