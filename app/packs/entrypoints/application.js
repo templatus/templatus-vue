58665958b277819d494cca4d5071266f874454de
Georg Ledermann <georg@ledermann.dev>
2021-05-22 15:05:35 +0200

    Add ServiceWorker

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -10,6 +10,29 @@ import Layout from '../src/Layout.vue';
 
 import 'stylesheets/application.css';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then((registration) => {
+      // console.log('Service worker registered successfully.', registration);
+      //
+      // var serviceWorker;
+      // if (registration.installing) {
+      //   serviceWorker = registration.installing;
+      //   console.log('Service worker installing.', serviceWorker);
+      // } else if (registration.waiting) {
+      //   serviceWorker = registration.waiting;
+      //   console.log('Service worker installed & waiting.', serviceWorker);
+      // } else if (registration.active) {
+      //   serviceWorker = registration.active;
+      //   console.log('Service worker active.', serviceWorker);
+      // }
+    })
+    .catch((error) => {
+      console.error('Service worker registration failed.', error);
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const app = createApp(Layout);
   app.use(router);
---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (event) => {
+  // console.log('Service worker has been installed.', event);
+});
+
+self.addEventListener('activate', event => {
+  // console.log('Service worker has been activated.', event);
+});
+
+self.addEventListener('fetch', event => {
+  // console.log('Service worker is fetching', event);
+});
