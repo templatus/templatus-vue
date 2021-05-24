995108c7e8125eb878d80faca3c8a8bd7c9c4f13
Georg Ledermann <georg@ledermann.dev>
2021-05-24 15:42:55 +0200

    Improve about page

--- README.md
+++ README.md
@@ -72,28 +72,25 @@ assets by path media/images/ 4.45 KiB
   assets by path media/images/*.br 1020 bytes
     asset media/images/bcb6d75d927347158af5.svg.br 796 bytes [emitted] [immutable] [compressed]
     asset media/images/f04dfe30a8ad8eb5c4e0.svg.br 224 bytes [emitted] [immutable] [compressed]
-assets by path js/*.js 113 KiB
-  asset js/448-1e62e0b7d2abc0b13ae3.js 101 KiB [emitted] [immutable] [minimized] (id hint: vendors) 4 related assets
-  asset js/application-8645c00747e82757d4ea.js 10.7 KiB [emitted] [immutable] [minimized] (name: application) 3 related assets
+assets by path js/*.js 117 KiB
+  asset js/536-cc83944ffbea040befe5.js 101 KiB [emitted] [immutable] [minimized] (id hint: vendors) 4 related assets
+  asset js/application-0a81eaf0192827aec2a8.js 14.3 KiB [emitted] [immutable] [minimized] (name: application) 3 related assets
   asset js/runtime-70d74373032b7674361d.js 1.39 KiB [emitted] [immutable] [minimized] (name: runtime) 3 related assets
-asset css/application-d6c0cdd4.css 16.5 KiB [emitted] [immutable] [minimized] (name: application) 3 related assets
+asset css/application-80be051f.css 17.3 KiB [emitted] [immutable] [minimized] (name: application) 3 related assets
 asset manifest.json 3.01 KiB [emitted] 2 related assets
-Entrypoint application 130 KiB (864 KiB) = js/runtime-70d74373032b7674361d.js 1.39 KiB js/448-1e62e0b7d2abc0b13ae3.js 101 KiB css/application-d6c0cdd4.css 16.5 KiB js/application-8645c00747e82757d4ea.js 10.7 KiB 6 auxiliary assets
-orphan modules 26.8 KiB [orphan] 43 modules
+Entrypoint application 134 KiB (871 KiB) = js/runtime-70d74373032b7674361d.js 1.39 KiB js/536-cc83944ffbea040befe5.js 101 KiB css/application-80be051f.css 17.3 KiB js/application-0a81eaf0192827aec2a8.js 14.3 KiB 6 auxiliary assets
+orphan modules 176 KiB [orphan] 273 modules
 runtime modules 3.36 KiB 7 modules
-code generated modules 612 KiB (javascript) 2.33 KiB (asset) 25.2 KiB (css/mini-extract) [code generated]
-  javascript modules 612 KiB
-    modules by path ./node_modules/@vue/ 374 KiB
-      ./node_modules/@vue/runtime-dom/dist/runtime-dom.esm-bundler.js 47.2 KiB [built] [code generated]
-      ./node_modules/@vue/runtime-core/dist/runtime-core.esm-bundler.js 279 KiB [built] [code generated]
-      ./node_modules/@vue/shared/dist/shared.esm-bundler.js 18.7 KiB [built] [code generated]
-      ./node_modules/@vue/reactivity/dist/reactivity.esm-bundler.js 29.7 KiB [built] [code generated]
-    7 modules
-  asset modules 84 bytes (javascript) 2.33 KiB (asset)
-    ./app/packs/images/rails.svg 42 bytes (javascript) 1.96 KiB (asset) [built] [code generated]
-    ./app/packs/images/vue.svg 42 bytes (javascript) 375 bytes (asset) [built] [code generated]
-  css ./node_modules/css-loader/dist/cjs.js??clonedRuleSet-3.use[1]!./node_modules/postcss-loader/dist/cjs.js??clonedRuleSet-3.use[2]!./app/packs/stylesheets/application.css 25.2 KiB [code generated]
-webpack 5.37.1 compiled successfully in 8645 ms
+code generated modules 616 KiB (javascript) 2.33 KiB (asset) 26.2 KiB (css/mini-extract) [code generated]
+  modules by path ./node_modules/ 592 KiB
+    modules by path ./node_modules/@vue/ 374 KiB 4 modules
+  modules by path ./app/packs/ 23.6 KiB (javascript) 2.33 KiB (asset) 26.2 KiB (css/mini-extract)
+    asset modules 84 bytes (javascript) 2.33 KiB (asset)
+      ./app/packs/images/rails.svg 42 bytes (javascript) 1.96 KiB (asset) [built] [code generated]
+      ./app/packs/images/vue.svg 42 bytes (javascript) 375 bytes (asset) [built] [code generated]
+    ./app/packs/entrypoints/application.js + 24 modules 23.6 KiB [built] [code generated]
+    css ./node_modules/css-loader/dist/cjs.js??clonedRuleSet-3.use[1]!./node_modules/postcss-loader/dist/cjs.js??clonedRuleSet-3.use[2]!./app/packs/stylesheets/application.css 26.2 KiB [code generated]
+webpack 5.37.1 compiled successfully in 8790 ms
 ```
 
-=> 113 KB JavaScript (minified, uncompressed)
+=> 117 KB JavaScript (minified, uncompressed)
--- app/packs/src/pages/About.vue
+++ app/packs/src/pages/About.vue
@@ -1,146 +1,238 @@
 <template>
-  <h1 class="text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
+  <h1
+    class="
+      text-4xl
+      font-extrabold
+      tracking-tight
+      sm:text-5xl
+      lg:text-6xl
+      text-red-800
+    "
+  >
     About
   </h1>
 
-  <p class="mt-6 text-xl max-w-3xl">
-    This is an opinionated template for using <b>Ruby on Rails</b> with
-    <b>Vue.js</b>
-  </p>
+  <div class="mt-6 mb-20 text-xl max-w-3xl">
+    <p>
+      This is an opinionated template for web applications with
+      <b>Ruby on Rails</b> and <b>Vue.js</b>
+    </p>
+  </div>
 
-  <hr class="my-10 mx-20" />
-
-  <div
-    class="
-      mt-10
-      grid grid-cols-1
-      text-xl
-      space-y-8
-      sm:grid-cols-4
-      sm:space-y-0 sm:space-x-10
-    "
-  >
-    <div v-for="ingredient in ingredients" :key="ingredient.name">
-      <h2 class="font-bold">
-        {{ ingredient.name }}
-      </h2>
-
-      <ul class="mt-3 leading-8">
-        <li v-for="item in ingredient.items" :key="item.name">
-          <a
-            class="hover:underline"
-            :href="item.href"
-            target="_blank"
-            rel="noopener"
+  <div class="space-y-24">
+    <div v-for="group in features" :key="group.name">
+      <div class="lg:grid lg:grid-cols-3 lg:gap-x-8">
+        <div>
+          <h2
+            class="text-base font-semibold text-red-800 uppercase tracking-wide"
           >
-            {{ item.name }}
-          </a>
-        </li>
-      </ul>
+            {{ group.slogan }}
+          </h2>
+          <p class="mt-2 text-3xl font-extrabold text-gray-900">
+            {{ group.name }}
+          </p>
+          <p class="mt-4 text-lg text-gray-500">
+            {{ group.description }}
+          </p>
+        </div>
+        <div class="mt-12 lg:mt-0 lg:col-span-2">
+          <dl
+            class="
+              space-y-10
+              sm:space-y-0
+              sm:grid sm:grid-cols-2
+              sm:gap-x-6 sm:gap-y-10
+              lg:gap-x-8
+            "
+          >
+            <div
+              v-for="feature in group.items"
+              :key="feature.name"
+              class="relative"
+            >
+              <dt>
+                <CheckIcon
+                  class="absolute h-6 w-6 text-green-500"
+                  aria-hidden="true"
+                />
+                <p class="ml-9 text-lg leading-6 font-medium text-red-800">
+                  <a
+                    :href="feature.href"
+                    class="hover:underline"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    {{ feature.name }}
+                  </a>
+                </p>
+              </dt>
+              <dd class="mt-2 ml-9 text-base text-gray-500">
+                {{ feature.description }}
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { CheckIcon } from '@heroicons/vue/outline';
 
-const ingredients = [
+const features: {
+  name: string;
+  slogan: string;
+  description: string;
+  items: { name: string; href: string; description: string }[];
+}[] = [
   {
-    name: 'Backend',
+    name: 'The backend',
+    slogan: 'Server side',
+    description: 'Data access layer',
     items: [
       {
         name: 'Ruby 3',
         href: 'https://www.ruby-lang.org/',
+        description:
+          'A dynamic, open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural to read and easy to write.',
       },
       {
         name: 'Ruby on Rails 6.1',
         href: 'https://rubyonrails.org/',
+        description:
+          'Rails is a web application development framework written in the Ruby programming language. It is designed to make programming web applications easier by making assumptions about what every developer needs to get started.',
       },
       {
         name: 'ActionCable',
         href: 'https://guides.rubyonrails.org/action_cable_overview.html',
+        description:
+          "Action Cable seamlessly integrates WebSockets with the rest of your Rails application. It allows for real-time features to be written in Ruby in the same style and form as the rest of your Rails application, while still being performant and scalable. It's a full-stack offering that provides both a client-side JavaScript framework and a server-side Ruby framework.",
       },
       {
         name: 'PostgreSQL',
         href: 'https://www.postgresql.org/',
+        description:
+          'PostgreSQL is a powerful, open source object-relational database system with over 30 years of active development that has earned it a strong reputation for reliability, feature robustness, and performance.',
+      },
+      {
+        name: 'Redis',
+        href: 'https://redis.io/',
+        description:
+          'Redis is an open source, in-memory data structure store, used as a database, cache, and message broker.',
       },
     ],
   },
+
   {
-    name: 'Frontend',
+    name: 'The frontend',
+    slogan: 'Client side',
+    description: 'Presentation layer',
     items: [
       {
         name: 'Vue 3',
         href: 'https://v3.vuejs.org/',
+        description:
+          'Vue is a progressive framework for building user interfaces.',
       },
       {
         name: 'TailwindCSS 2',
         href: 'https://tailwindcss.com/',
+        description:
+          'A utility-first CSS framework packed with classes that can be composed to build any design, directly in your markup.',
       },
       {
         name: 'TypeScript',
         href: 'https://www.typescriptlang.org/',
+        description:
+          'TypeScript is an open-source language which builds on JavaScript, one of the world’s most used tools, by adding static type definitions.',
       },
       {
         name: 'Vuex',
         href: 'https://vuex.vuejs.org/',
+        description:
+          'Vuex is a state management pattern + library for Vue.js applications. It serves as a centralized store for all the components in an application, with rules ensuring that the state can only be mutated in a predictable fashion.',
       },
       {
         name: 'HeadlessUI',
         href: 'https://headlessui.dev/',
+        description:
+          'Completely unstyled, fully accessible UI components, designed to integrate beautifully with Tailwind CSS.',
       },
       {
         name: 'HeroIcons',
         href: 'https://heroicons.com/',
+        description:
+          'Beautiful hand-crafted SVG icons, by the makers of Tailwind CSS.',
       },
       {
-        name: 'Webpack 5',
-        href: 'https://webpack.js.org/',
+        name: 'Webpacker',
+        href: 'https://github.com/rails/webpacker',
+        description:
+          'Webpacker makes it easy to use the JavaScript pre-processor and bundler Webpack v5 to manage application-like JavaScript in Rails.',
       },
     ],
   },
 
   {
     name: 'Continuous integration',
+    slogan: 'Quality insurance',
+    description: 'All about testing and building',
     items: [
       {
         name: 'GitHub Actions',
         href: 'https://github.com/features/actions',
+        description: 'Test, build and deliver on every push to GitHub',
       },
       {
         name: 'RSpec',
         href: 'https://rspec.info/',
+        description:
+          'Behaviour Driven Development for Ruby. Making TDD Productive and Fun.',
       },
       {
         name: 'RuboCop',
         href: 'https://rubocop.org/',
+        description: 'The Ruby Linter/Formatter that Serves and Protects',
       },
       {
         name: 'Jest',
         href: 'https://jestjs.io/',
+        description:
+          'Jest is a delightful JavaScript Testing Framework with a focus on simplicity',
       },
       {
         name: 'ESLint',
         href: 'https://eslint.org/',
+        description:
+          'ESLint statically analyzes your code to quickly find problems',
       },
     ],
   },
 
   {
     name: 'Deployment',
+    slogan: 'Ship it!',
+    description: 'Running in production',
     items: [
       {
         name: 'Docker',
         href: 'https://www.docker.com/',
+        description: 'Dockerized using an optimized Alpine base image.',
       },
     ],
   },
 ];
 
 export default defineComponent({
+  components: {
+    CheckIcon,
+  },
+
   setup() {
     return {
-      ingredients,
+      features,
     };
   },
 });
