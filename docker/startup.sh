2b142990b83fe94241bc31f987c0f59236b6db99
Georg Ledermann <georg@ledermann.dev>
2021-05-30 12:35:46 +0200

    Docker: Wait for Redis on startup

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,6 +4,14 @@ echo "Starting ..."
 echo "Git commit: $COMMIT_SHA - $COMMIT_TIME"
 echo "----------------"
 
+# Wait for Redis
+until nc -z -v -w30 $REDIS_HOST 6379
+do
+  echo "Waiting for Redis..."
+  sleep 1
+done
+echo "Redis is up and running!"
+
 # Wait for PostgreSQL
 until nc -z -v -w30 $DB_HOST 5432
 do
