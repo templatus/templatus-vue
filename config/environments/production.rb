8c58d8f5d9df65963d2e521f93d0c0d448bfa187
Georg Ledermann <georg@ledermann.dev>
2021-06-04 07:40:25 +0200

    Revert "ActionCable: Add config"
    This reverts commit aba44eb9d41b6c10885951a80cd097151a4452b8.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,9 +47,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  config.action_cable.mount_path = '/cable'
-  config.action_cable.url = "wss://#{ENV['APP_HOST']}/cable"
-  config.action_cable.allowed_request_origins = [ENV['APP_HOST']]
+  # config.action_cable.mount_path = nil
+  # config.action_cable.url = 'wss://example.com/cable'
+  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
