644f97eae6231d2bfbee8d1ffe112603135cc333
Georg Ledermann <georg@ledermann.dev>
2021-08-21 08:35:05 +0200

    Update webpacker and fix HMR

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const webpackConfig = require('./base');
 
+// LiveReload should be disabled when HMR is enabled
+webpackConfig.devServer.liveReload = !webpackConfig.devServer.hot
+console.log(webpackConfig.devServer)
+
 module.exports = webpackConfig;
