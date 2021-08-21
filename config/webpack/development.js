aef391eb4d673ce457ab6cd738148ee17eae266a
Georg Ledermann <georg@ledermann.dev>
2021-08-21 08:46:34 +0200

    Cleanup

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -4,6 +4,5 @@ const webpackConfig = require('./base');
 
 // LiveReload should be disabled when HMR is enabled
 webpackConfig.devServer.liveReload = !webpackConfig.devServer.hot
-console.log(webpackConfig.devServer)
 
 module.exports = webpackConfig;
