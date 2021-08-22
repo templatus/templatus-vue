58045e63fc5ccd4441fa0fd662ded207a3319485
Georg Ledermann <georg@ledermann.dev>
2021-08-22 07:06:51 +0200

    Update webpacker and remove liveReload hack

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,7 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const webpackConfig = require('./base');
 
-// LiveReload should be disabled when HMR is enabled
-webpackConfig.devServer.liveReload = !webpackConfig.devServer.hot
-
 module.exports = webpackConfig;
