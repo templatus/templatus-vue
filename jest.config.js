28abdffc61c4c046b758900848f8cc4aa9d6f52e
Georg Ledermann <georg@ledermann.dev>
2021-09-17 06:01:34 +0200

    Jest: Use jest-transform-stub for file stubs

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/app/packs/src/$1',
     '^@test/(.*)$': '<rootDir>/spec/packs/src/$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/spec/packs/__mocks__/fileMock.js',
+      'jest-transform-stub',
   },
   moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
   preset: 'ts-jest',
