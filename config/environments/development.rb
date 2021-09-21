0f11b0fddab445c67c5d6a4aa05b4c78005a0baf
Georg Ledermann <georg@ledermann.dev>
2021-09-21 17:31:07 +0200

    In development, redirect localhost to APP_HOST

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,6 +70,12 @@ Rails.application.configure do
   config.force_ssl =
     ActiveModel::Type::Boolean.new.cast ENV.fetch('FORCE_SSL', true)
 
+  if config.force_ssl
+    # Ensure that http://localhost:3000 redirects to https://{APP_HOST},
+    # because there is no https://localhost:3000
+    config.ssl_options = { redirect: { host: ENV['APP_HOST'], port: 80 } }
+  end
+
   # Use https for webpack-dev-server (requires puma-dev)
   config.action_controller.asset_host = "https://webpack.#{ENV['APP_HOST']}"
 
