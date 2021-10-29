a7f2c7a2ff89ea342df56aa0bfbacd6367e6198b
Georg Ledermann <georg@ledermann.dev>
2021-10-29 06:22:24 +0200

    Reformat with Prettier

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
-Rails
-  .application
-  .routes
-  .draw do
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-    mount Sidekiq::Web => '/sidekiq'
+  mount Sidekiq::Web => '/sidekiq'
 
-    resources :clicks, only: %i[index create]
-    root to: 'vue#index'
-  end
+  resources :clicks, only: %i[index create]
+  root to: 'vue#index'
+end
--- public/sw.js
+++ public/sw.js
@@ -2,10 +2,10 @@ self.addEventListener('install', (_event) => {
   // console.log('sw.js: Service worker has been installed.', event);
 });
 
-self.addEventListener('activate', _event => {
+self.addEventListener('activate', (_event) => {
   // console.log('sw.js: Service worker has been activated.', event);
 });
 
-self.addEventListener('fetch', _event => {
+self.addEventListener('fetch', (_event) => {
   // console.log('sw.js: Service worker is fetching', event);
 });
