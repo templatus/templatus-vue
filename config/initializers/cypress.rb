c15cfe3e22578f8acbdd45df5d317a10090567bc
Georg Ledermann <georg@ledermann.dev>
2021-12-03 10:37:16 +0100

    Use default initializer

--- config/initializers/cypress.rb
+++ config/initializers/cypress.rb
@@ -3,8 +3,7 @@ return unless Rails.env.test?
 Rails.application.load_tasks unless defined?(Rake::Task)
 
 CypressRails.hooks.before_server_start do
-  # Add our fixtures before the resettable transaction is started
-  Rake::Task['db:fixtures:load'].invoke
+  # Called once, before either the transaction or the server is started
 end
 
 CypressRails.hooks.after_server_start do
@@ -16,10 +15,9 @@ CypressRails.hooks.after_transaction_start do
 end
 
 CypressRails.hooks.after_state_reset do
-  # After state reset
+  # Triggered after `/cypress_rails_reset_state` is called
 end
 
 CypressRails.hooks.before_server_stop do
-  # Purge and reload the test database so we don't leave our fixtures in there
-  Rake::Task['db:test:prepare'].invoke
+  # Called once, at_exit
 end
