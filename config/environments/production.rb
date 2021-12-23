9a17b604a4dc012423b9a8696c675c6f815ee23c
Georg Ledermann <georg@ledermann.dev>
2021-12-23 09:05:15 +0100

    Cleanup asset config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,14 +28,20 @@ Rails.application.configure do
     'Cache-Control' => 'public, s-maxage=31536000, max-age=31536000, immutable',
   }
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.asset_host = ENV.fetch('ASSET_HOST', nil).presence
+  # Compress CSS using a preprocessor.
+  # config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
 
   # Compress assets with Brotli
   config.assets.configure do |env|
     env.register_exporter %w[text/css application/javascript image/svg+xml],
                           Sprockets::ExportersPack::BrotliExporter
   end
+
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  config.asset_host = ENV.fetch('ASSET_HOST', nil).presence
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
