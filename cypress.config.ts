51a1d36a249906fcaf2c24a63d6fea3a780481c6
Georg Ledermann <georg@ledermann.dev>
2022-06-02 08:34:22 +0200

    :arrow_up: Upgrade Cypress to 10.0.1

---
+++ cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  projectId: '5d6bqs',
+  screenshotsFolder: 'tmp/cypress_screenshots',
+  trashAssetsBeforeRuns: false,
+  videosFolder: 'tmp/cypress_videos',
+  fixturesFolder: 'spec/cypress/fixtures',
+  downloadsFolder: 'spec/cypress/downloads',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./spec/cypress/plugins/index.js')(on, config);
+    },
+    baseUrl: 'https://templatus.test',
+    specPattern: 'spec/cypress/integration/**/*.{js,jsx,ts,tsx}',
+    supportFile: 'spec/cypress/support/index.js',
+  },
+});
